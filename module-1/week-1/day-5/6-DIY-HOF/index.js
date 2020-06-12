b6e01ca8a6bcd38e6fa87b77d2550a7c8e4105be
KostraOne <guillaume.amangoua@gmail.com>
2020-06-12 13:18:33 +0200

    get confident with HOF

---
+++ module-1/week-1/day-5/6-DIY-HOF/index.js
@@ -0,0 +1,21 @@
+// *************
+// D.I.Y - H.O.F
+// *************
+
+const products = [
+  { id: 1, name: "smart phone", price: 400, stock: 0 },
+  { id: 2, name: "smart watch", price: 300, stock: 100 },
+  { id: 3, name: "smart frigo", price: 650, stock: 10 },
+  { id: 4, name: "smart home", price: 1000000, stock: 0 },
+  { id: 5, name: "tablet Samsung", price: 420, stock: 1000 },
+  { id: 6, name: "iphone", price: 1000, stock: 40 },
+  { id: 7, name: "ipad", price: 500, stock: 0 },
+];
+
+// A - use forEach to print each product in the console
+// B - use forEach to print the name of each product with a price that is less than 500
+// C - use filter to create a new array containing only the products with stock
+// D - use map to create a new array of strings, containing the product's names in uppercase
+// E - use Array.sort() to sort the products by price (ascending order)
+// F - use Array.sort() to sort the products by id (descending order)
+// G - use reduce to get the sum of all product's prices
