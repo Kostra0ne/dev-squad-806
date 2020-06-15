960023d377b0e96c04d9db23bca7ba53bb8af277
KostraOne <guillaume.amangoua@gmail.com>
2020-06-15 14:28:57 +0200

    added class toggle on diy dom loops

--- module-1/week-2/day-1/7-DIY-DOM+loops/app.js
+++ module-1/week-2/day-1/7-DIY-DOM+loops/app.js
@@ -1,18 +1,28 @@
 // target usefull element(s) (used at several locations)
-const blocks = document.querySelectorAll(".block");
+const blocks = document.querySelectorAll(".block"); // this will return a nodeList of all matching nodes
 
 // this functions prints one block's info
 function printBlockInfo(block) {
-  console.log(`id: ${block.id} - CSS classes: ${block.className}`);
+  console.log(`id: ${block.id} - CSS classes: ${block.className}`); // just a log ...
 }
 
 // get one element by id and change it's text-content
 function printBlockCount() {
-  const resElement = document.getElementById("blocks-count");
-  resElement.textContent = `This document contains ${blocks.length} blocks`;
+  const resElement = document.getElementById("blocks-count"); // target an element by it's id
+  resElement.textContent = `This document contains ${blocks.length} blocks`;//update the text content
+}
+
+function toggleBackground(evt) {
+  // evt.target represents the clicked block
+  evt.target.classList.toggle("is-selected"); // add OR remove the is-selected css class
 }
 
 // loop through all the block
-blocks.forEach((b) => printBlockInfo(b)); // pass each block to the printBlockInfo function
+blocks.forEach((b) => { 
+  b.onclick = toggleBackground; // on click on each block, run this callback
+  printBlockInfo(b); // pass each block to the printBlockInfo function
+}); 
 
-printBlockCount(); 
+printBlockCount(); // run the function to display the length
+
+
