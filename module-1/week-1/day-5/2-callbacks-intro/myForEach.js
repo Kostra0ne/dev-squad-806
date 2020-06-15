0f1018df35beddc25c06a95afad8716ded54e6a5
KostraOne <guillaume.amangoua@gmail.com>
2020-06-15 13:36:34 +0200

    added comments and minor changes

--- module-1/week-1/day-5/2-callbacks-intro/myForEach.js
+++ module-1/week-1/day-5/2-callbacks-intro/myForEach.js
@@ -1,9 +1,12 @@
 Array.prototype.myForEach = function (clbk) {
+  // in this case, "this" represents the full array
   for (let i = 0; i < this.length; i += 1) {
     clbk(this[i], i, this);
   }
 };
 
-["a", "b", "c", "d"].myForEach(function (currentValue, index, fullArray) {
+const arr = ["a", "b", "c", "d"];
+
+arr.myForEach(function (currentValue, index, fullArray) {
   console.log(currentValue, index, fullArray);
 });
