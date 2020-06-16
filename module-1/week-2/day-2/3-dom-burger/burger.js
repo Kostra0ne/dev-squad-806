905b8c9177c0f08c474a32e70befb695db52e437
KostraOne <guillaume.amangoua@gmail.com>
2020-06-16 13:03:58 +0200

    finished burger toggle example

--- module-1/week-2/day-2/3-dom-burger/burger.js
+++ module-1/week-2/day-2/3-dom-burger/burger.js
@@ -1,3 +1,12 @@
 // get the burger by id
 // listen to click on it
 // the callback will toggle a is active class on a secondary mobile navbar
+
+const burger = document.getElementById("burger");
+const navMobile = document.getElementById("nav-mobile");
+
+function toggleNavMobile() {
+  navMobile.classList.toggle("is-active");
+}
+
+burger.onclick = toggleNavMobile;
