8378dcb59cc70a1102f75e744ad3b8f5181ce5a1
KostraOne <guillaume.amangoua@gmail.com>
2020-07-01 10:33:27 +0200

    added a redo-do express app with css vars and .env file

---
+++ module-2/week-1/day-3/express-redo-do/app.js
@@ -0,0 +1,22 @@
+// below : extract the .env content + expose it in a node sytem object called process.env
+// process.env is just an object where we store usefull infos
+// process.env is private, don't push it on github !!!
+require("dotenv").config();
+
+const express = require("express");
+const hbs = require("hbs");
+const app = express();
+
+// base config
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.listen(process.env.PORT, function () {
+  console.log(`My app is ready to rock @ http://localhost:${process.env.PORT}!`);
+});
