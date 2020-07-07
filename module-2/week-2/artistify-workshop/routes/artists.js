fe112d73d7976d3366c433006115a19729b169d5
KostraOne <guillaume.amangoua@gmail.com>
2020-07-07 14:52:14 +0200

    added new files for styles linking with artists

---
+++ module-2/week-2/artistify-workshop/routes/artists.js
@@ -0,0 +1,112 @@
+const express = require("express");
+const router = new express.Router();
+const artistModel = require("../models/Artist");
+const styleModel = require("../models/Style");
+const uploader = require("./../config/cloudinary");
+
+// *********************************************
+// ALL THESE ROUTES ARE PREFIXED WITh "/artists"
+// *********************************************
+
+// PUBLIC ROUTES
+
+router.get("/all", (req, res, next) => {
+  artistModel
+    .find() // retreive all the documents in the artists collection
+    .populate("style")
+    .then((dbResults) => {
+      console.log(dbResults);
+
+      res.render("lists/artists", {
+        artists: dbResults,
+      });
+    })
+    .catch(next);
+});
+
+router.get("/page/:id", (req, res, next) => {
+  artistModel
+    .findById(req.params.id)
+    .then((artist) => {
+      res.render("page-artist", { artist });
+    })
+    .catch(next);
+});
+
+// PRIVATE ROUTES
+
+router.get("/admin", (req, res, next) => {
+  artistModel
+    .find() // retreive all the documents in the artists collection
+    .populate("style")
+    .then((dbResults) => {
+      res.render("tables/artists", {
+        artists: dbResults,
+      });
+    })
+    .catch(next);
+});
+
+router.get("/create", async (req, res) => {
+  try {
+    res.render("forms/artist", { styles: await styleModel.find() });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/create", uploader.single("picture"), (req, res, next) => {
+  const { name, description, isBand, style } = req.body;
+
+  artistModel
+    .create({
+      name,
+      description,
+      isBand: isBand === "yes",
+      style,
+      picture: req.file ? req.file.path : undefined,
+    })
+    .then(() => {
+      req.flash("success", "artist successfully created");
+      res.redirect("/artists/admin");
+    })
+    .catch(next);
+});
+
+router.get("/update/:id", (req, res, next) => {
+  Promise.all([
+    artistModel.findById(req.params.id).populate("style"),
+    styleModel.find()
+  ])
+    .then((dbRes) => {
+      res.render("forms/artist-update", { artist: dbRes[0], styles: dbRes[1] });
+    })
+    .catch(next);
+});
+
+router.post("/update/:id", uploader.single("picture"), (req, res, next) => {
+  const updatedArtist = req.body;
+
+  if (req.file) updatedArtist.picture = req.file.path;
+
+  (updatedArtist.isBand = updatedArtist.isBand === "yes"),
+    artistModel
+      .findByIdAndUpdate(req.params.id, updatedArtist)
+      .then(() => {
+        req.flash("success", "artist successfully updated");
+        res.redirect("/artists/admin");
+      })
+      .catch(next);
+});
+
+router.get("/delete/:id", (req, res, next) => {
+  artistModel
+    .findByIdAndDelete(req.params.id)
+    .then((dbRes) => {
+      req.flash("success", "artist successfully deleted");
+      res.redirect("/artists/admin");
+    })
+    .catch(next);
+});
+
+module.exports = router;
