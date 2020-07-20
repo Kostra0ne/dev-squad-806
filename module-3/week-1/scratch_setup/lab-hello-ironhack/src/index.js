bab2bbcf9bf7d552955e16dabb6aeb990afaac3b
yeeeehaw <frankomarmier@gmail.com>
2020-07-20 10:03:42 +0200

    Module 3

---
+++ module-3/week-1/scratch_setup/lab-hello-ironhack/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./App.css";
+
+ReactDOM.render(
+  <div>
+    <div className="toto">text</div>
+    <img src="/images/icon1.png" />
+    <div>feahfafa</div>
+  </div>,
+  document.getElementById("root")
+);
